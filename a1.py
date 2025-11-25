c393b2cfcb4bf32e275fc2970b2ef345b315aea9
Ukashah2012 <ukashtik@gmail.com>
2025-11-25 18:13:14 +0500

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,4 @@
+print("Welcome to python project!")
+print(100)
+print("100+200=" ,100+200)
+print("I purchased book for" , 500, end="$")
---
+++ a2.py
@@ -0,0 +1,6 @@
+n1= input("Enter number1=")
+n2= input("Enter number2=")
+sum=n1+n2
+print("sum=" ,sum)
+
+
